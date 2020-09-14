4f32b5fbf32854c62fdbd377c4b63bc67d33fc37
Jerkbert <raballantyne@gmail.com>
2020-09-14 12:27:53 -0600

    buildSchema method

--- src/main/java/com/graphqljava/tutorial/bookdetails/GraphQLProvider.java
+++ src/main/java/com/graphqljava/tutorial/bookdetails/GraphQLProvider.java
@@ -1,6 +1,25 @@
 @Component
 public class GraphQLProvider {
 
+  @Autowired
+  GraphQLDataFetchers graphQLDataFetchers;
+
+  private GraphQLSchema buildSchema(String sdl) {
+    TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+    RuntimeWiring runtimeWiring = buildWiring();
+    SchemaGenerator schemaGenerator = new SchemaGenerator();
+    return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+  }
+
+  private RuntimeWiring buildWiring() {
+    return RuntimeWiring.newRuntimeWiring()
+      .type(newTypeWiring("Query")
+          .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+      .type(newTypeWiring("Book")
+          .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher()))
+      .build();
+  }
+
   private GraphQL graphQL;
   
   @Bean
